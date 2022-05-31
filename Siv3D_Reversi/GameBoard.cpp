8a89b673076d45604fc84fdb7c3f31a696136b83
naga-naga <kiyoharu.639721@gmail.com>
2022-05-31 17:18:33 +0900

    石を描画できるようになった

--- Siv3D_Reversi/GameBoard.cpp
+++ Siv3D_Reversi/GameBoard.cpp
@@ -1,24 +1,54 @@
 ﻿#include "GameBoard.hpp"
 
+GameBoard::GameBoard() {
+    stones[4][5] = Black;
+    stones[5][4] = Black;
+    stones[4][4] = White;
+    stones[5][5] = White;
+}
+
 void GameBoard::draw() const {
     drawGridLine();
+    drawCells();
 }
 
 // 格子を描く
 void GameBoard::drawGridLine() const {
-    for (auto i : step(9)) {
+    for (int32 i = 1; i <= 9; i++) {
         // 縦線
-        Line{ (i * CellSize), 0, (i * CellSize), (8 * CellSize) }.draw(4, lineColor);
+        Line{ (i * CellSize), CellSize, (i * CellSize), (9 * CellSize) }.draw(4, lineColor);
 
         // 横線
-        Line{ 0, (i * CellSize), (8 * CellSize), (i * CellSize) }.draw(4, lineColor);
+        Line{ CellSize, (i * CellSize), (9 * CellSize), (i * CellSize) }.draw(4, lineColor);
 
     }
 
     // 黒点
-    for (auto i : { 2, 6 }) {
-        for (auto j : { 2, 6 }) {
+    for (auto i : { 3, 7 }) {
+        for (auto j : { 3, 7 }) {
             Circle{ i * CellSize, j * CellSize, 7 }.draw(lineColor);
+        }
+    }
+}
+
+void GameBoard::drawCells() const {
+    // 各セルを見ていき，石を描画する．
+    for (int32 i = 1; i <= 8; i++) {
+        for (int32 j = 1; j <= 8; j++) {
+            // セル
+            const Rect cell{ (i * CellSize), (j * CellSize), CellSize };
+
+            // セルの石
+            const int32 stone = stones[j][i];
+
+            // 石を描画
+            if (stone == Black) {
+                Circle{ cell.center(), CellSize * 0.4 }.draw(ColorF{ Palette::Black });
+                continue;
+            } else if (stone == White) {
+                Circle{ cell.center(), CellSize * 0.4 }.draw(ColorF{ Palette::White });
+                continue;
+            }
         }
     }
 }
--- Siv3D_Reversi/GameBoard.hpp
+++ Siv3D_Reversi/GameBoard.hpp
@@ -5,10 +5,23 @@ class GameBoard {
 public:
     static constexpr int32 CellSize = 50;
 
+    static constexpr int32 Black = 1;
+
+    static constexpr int32 White = -1;
+
+	GameBoard();
+
     void draw() const;
 
 private:
+    // 置かれた石を表す
+    // 角での計算を楽にするため，上下左右1マスずつ大きくとる
+    // 実際には 1 ~ 8 を使う
+    Grid<int32> stones = Grid<int32>(10, 10);
+
     ColorF lineColor = ColorF{ 0.2 };
 
     void drawGridLine() const;
+
+    void drawCells() const;
 };
